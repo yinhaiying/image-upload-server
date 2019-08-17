5ac9ec84e200e24161ddcb5b79023220c8b5283e
yinhaiying <188408425712163.com>
2019-08-17 15:48:25 +0800

    从env中获取端口号

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ app.get('/upload/:key', cors(), function(req, res, next){
     }
   })
 })
+// Heroku的端口号是随机给的，因此我们不能直接写3000；
+let port = process.env.PORT || 3000;
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`)
 })
